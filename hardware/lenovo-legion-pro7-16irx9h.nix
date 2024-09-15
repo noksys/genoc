7cfaaa6c4f1d0dfeec048b494c56727019867fff
Felipe Micaroni Lalli <micaroni@gmail.com>
2024-09-14 20:16:56 -0400

    Adding specific hardware config.

---
+++ hardware/lenovo-legion-pro7-16irx9h.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    # Don't forget to run:
+    # sudo nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
+
+    # sudo nix-channel --update
+
+    <nixos-hardware/lenovo/legion/16irx8h>
+    ./baremetal.nix
+  ];
+}
