bf8c6dc3ceaa4f75ef275bdf9367085ce85df250
Felipe Micaroni Lalli <micaroni@gmail.com>
2024-09-16 02:22:50 -0300

    Minor: removing empty line.

--- hardware/lenovo-legion-pro7-16irx9h.nix
+++ hardware/lenovo-legion-pro7-16irx9h.nix
@@ -4,7 +4,6 @@
   imports = [
     # Don't forget to run:
     # sudo nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
-
     # sudo nix-channel --update
 
     <nixos-hardware/lenovo/legion/16irx8h>
