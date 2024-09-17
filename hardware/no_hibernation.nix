bc6f925ac637ff2d6a0022e4c95c3fdf8df16b36
Felipe Micaroni Lalli <micaroni@gmail.com>
2024-09-17 19:14:38 -0300

    Adding option to disable sleep & hibernation because it has issues in some systems.

---
+++ hardware/no_hibernation.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  systemd = {
+    targets = {
+      sleep = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      suspend = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      hibernate = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+      "hybrid-sleep" = {
+        enable = false;
+        unitConfig.DefaultDependencies = "no";
+      };
+    };
+  };
+}
