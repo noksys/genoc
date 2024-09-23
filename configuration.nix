c0b622aa2a8e80b2219d0e1399781911f504c5ae
Felipe Micaroni Lalli <micaroni@gmail.com>
2024-09-23 20:06:00 -0300

    Inluding some common configs of Tor.

--- configuration.nix
+++ configuration.nix
@@ -128,8 +128,45 @@ in
   services.tor = {
     enable = true;
     enableGeoIP = false;
+    torsocks.enable = true;
+#    openFirewall = true;
     client.enable = true;
     relay.enable = false;
+
+#     relay = {
+#       enable = true;
+#       role = "relay";  # Set the relay role (e.g., "relay", "bridge")
+#     };
+
+    settings = {
+      #Nickname = "Lazy Guy";
+      #ContactInfo = "lazyguy@example.com";
+
+      MaxAdvertisedBandwidth = "10 MB";
+      BandWidthRate = "5 MB";
+      BandwidthBurst = "10 MB";
+      #RelayBandwidthRate = "5 MB";
+      #RelayBandwidthBurst = "10 MB";
+
+      # Restrict exit nodes to a specific country (use the appropriate country code)
+      #ExitNodes = "{ch} StrictNodes 1";
+
+      # Reject all exit traffic
+      ExitPolicy = ["reject *:*"];
+
+      # Performance and security settings
+      CookieAuthentication = true;
+      CookieAuthFileGroupReadable = true;
+      DataDirectoryGroupReadable = true;
+      AvoidDiskWrites = 1;
+      HardwareAccel = 1;
+      SafeLogging = 1;
+      ControlPort = 9051;
+      HashedControlPassword = "${vars.torControlPasswordHash}";
+
+      # Network settings
+      #ORPort = [443];
+    };
   };
 
   # To avoid any issues with Windows automatic sync time on dual boot machine
--- custom_vars.example.nix
+++ custom_vars.example.nix
@@ -9,5 +9,6 @@ in
   installationNixOSVersion = "24.05"; # what version did you use to make the first install?
   mainUser = mainUser;
   timeZone = "America/Sao_Paulo";
+  torControlPasswordHash = "16:97DE2B9B1F6198A46076DB9B879BF5291CC71B5CE6F27FE5CF4746C99C"; # tor --hash-password "xpto"
   userFullName = "Lazy Guy";
 }
