87c3f3c8d357b4610b695a30ffa325cbb2f9cfa3
Felipe Micaroni Lalli <micaroni@gmail.com>
2024-11-01 03:39:59 -0300

    Revert "Frustrated attempt."
    This reverts commit 980d0a44627ff0b6b3503d63aefb6031e637a71c.

--- boot/plymouth.nix
+++ boot/plymouth.nix
@@ -1,20 +1,57 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 let
   vars = import ../../custom_vars.nix;
-  customPlymouthTheme = pkgs.callPackage ./custom-plymouth-theme.nix { inherit vars; };
 in
 {
   boot = {
     plymouth = {
       enable = true;
       theme = "${vars.plymouthTheme}";
-      themePackages = [ customPlymouthTheme ];
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "${vars.plymouthTheme}" ];
+        })
+      ];
     };
+
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+
+    kernelParams = lib.mkDefault (config.boot.kernelParams ++ [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ]);
   };
 
-  environment.systemPackages = with pkgs; [
-    customPlymouthTheme
-    plymouth
+  system.activationScripts.copyPlymouthAssets = ''
+    themeDir="/etc/plymouth/themes/${vars.plymouthTheme}"
+    mkdir -p "$themeDir"
+    cp ${./nixos.png} "$themeDir/"
+    scriptFile="$themeDir/${vars.plymouthTheme}.script"
+    if [ -f "$scriptFile" ]; then
+      echo '
+      # display nixos logo
+      nixos_image = Image("nixos.png");
+      nixos_sprite = Sprite();
+      nixos_sprite.SetImage(nixos_image);
+      nixos_sprite.SetX(Window.GetX() + (Window.GetWidth() / 2 - nixos_image.GetWidth() / 2));
+      nixos_sprite.SetY(Window.GetHeight() - nixos_image.GetHeight() - 50);
+      ' >> "$scriptFile"
+    fi
+
+    plymouth-set-default-theme --reset --rebuild-initrd "${vars.plymouthTheme}" --theme-dir="/etc/plymouth/themes"
+  '';
+
+  environment.systemPackages = lib.mkMerge [
+    (with pkgs; [
+       adi1090x-plymouth-themes
+       plymouth
+    ])
   ];
 }
