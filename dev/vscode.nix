6ed808f7f9100fa374c3e4cb82446c7e0b6d444d
Felipe Micaroni Lalli <micaroni@gmail.com>
2025-07-22 01:06:23 -0300

    VSCode fix.

--- dev/vscode.nix
+++ dev/vscode.nix
@@ -1,28 +1,17 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-let
-  unstable = import <nixos-unstable> { config.allowUnfree = true; };
-in
 {
   nixpkgs.config = {
-    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-      "vscode-1.93.0"
-    ];
-
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "vscode" ];
     permittedInsecurePackages = [
       "openssl-1.1.1u"
       "qtwebkit-5.212.0-alpha4"
     ];
-
-    packageOverrides = pkgs: {
-      vscode = unstable.vscode;
-    };
   };
 
-  environment.systemPackages = lib.mkMerge [
-    (with pkgs; [
-      wmctrl
-      xorg.xprop
-      ])
+  environment.systemPackages = with pkgs; [
+    wmctrl
+    xorg.xprop
+    vscode
   ];
 }
