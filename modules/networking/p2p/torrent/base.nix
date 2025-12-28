12ca5ad8cd37f11b92c4bb79984f526352d738ab
Felipe Micaroni Lalli <micaroni@gmail.com>
2025-12-28 01:44:31 -0300

    Fix.

--- modules/networking/p2p/torrent/base.nix
+++ modules/networking/p2p/torrent/base.nix
@@ -3,6 +3,6 @@
 {
   environment.systemPackages = with pkgs; [
     transmission_4-gtk # BitTorrent client (GTK)
-    transmission_4-cli # CLI tools (transmission-remote)
+    transmission # CLI tools (transmission-remote)
   ];
 }
