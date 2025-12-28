7ed8ea13fa8b27f3e2a431c7065a5ae3223b177c
Felipe Micaroni Lalli <micaroni@gmail.com>
2025-12-28 06:36:58 -0300

    Removing Opera.

--- modules/web/browsers/exotic.nix
+++ modules/web/browsers/exotic.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [
     brave       # Brave browser
     vivaldi     # Vivaldi browser
-    opera       # Opera browser
+    # opera       # Opera browser / Deprecated for now
     nyxt        # Keyboard-driven browser
     qutebrowser # Keyboard-driven browser
     kdePackages.falkon # KDE browser
