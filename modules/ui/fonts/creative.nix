893d5ce98d0282278df6e19c4f19f3c4b4ec251f
Felipe Micaroni Lalli <micaroni@gmail.com>
2025-12-28 01:09:54 -0300

    Fix.

--- modules/ui/fonts/creative.nix
+++ modules/ui/fonts/creative.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 {
   fonts.packages = with pkgs; [
-    dancing-script
     google-fonts
-    libre-baskerville
-    libre-bodoni
-    libre-caslon
-    montserrat
-    open-sans
-    oswald
-    playfair-display
-    raleway
   ];
 }
