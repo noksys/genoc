eab14702c6a9261d7af4697c694fd3dee46c2db4
Felipe Micaroni Lalli <micaroni@gmail.com>
2025-12-28 02:39:14 -0300

    Fix.

--- policies/desktop/hybrid/rich-kde-primary.nix
+++ policies/desktop/hybrid/rich-kde-primary.nix
@@ -8,4 +8,7 @@
   # Force SDDM (KDE) as the display manager
   services.displayManager.sddm.enable = lib.mkForce true;
   services.displayManager.gdm.enable = lib.mkForce false;
+
+  # KDE primary: prefer ksshaskpass to avoid GNOME seahorse conflict
+  programs.ssh.askPassword = lib.mkForce "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
 }
